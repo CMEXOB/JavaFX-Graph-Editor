9621d5119f827a1d5729b267249883f609a2f4f7
Max <games.03@inbox.ru>
2023-09-18 23:17:55 +0300

    Add functions to DrawableObject

--- src/main/java/com/applic/entity/DrawableObject.java
+++ src/main/java/com/applic/entity/DrawableObject.java
@@ -21,4 +21,20 @@ public abstract class DrawableObject implements Drawable{
     public void addPoint(Point point){
         points.add(point);
     }
+    public boolean isContainPoint(Point point){
+        for(Point objectPoint : points){
+            if(objectPoint.getX().equals(point.getX()) && objectPoint.getY().equals(point.getY())){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isContainPoint(Integer x, Integer y){
+        for(Point objectPoint : points){
+            if(objectPoint.getX().equals(x) && objectPoint.getY().equals(y)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
